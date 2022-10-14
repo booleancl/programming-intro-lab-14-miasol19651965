a091777bc14dfe682b49b601739ad6097f10e70c
Maria <marinasol21_5@hotmail.cl>
2022-10-14 08:54:15 -0500

    comentarios

---
+++ migrations/3_createComments.sql
@@ -0,0 +1,17 @@
+\c blog
+
+
+DROP TABLE comments;
+
+CREATE TABLE comments(
+    id INTEGER,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    content VARCHAR(255),
+    date DATE,
+
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES posts(id)
+
+);
--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,7 @@
+\c blog
+
+\COPY users(email) FROM 'C:/Users/Usuaria/intro_dev/programming-intro-lab-14-miasol19651965/data/users.csv'csv HEADER;
+\COPY posts FROM 'C:/Users/Usuaria/intro_dev/programming-intro-lab-14-miasol19651965/data/posts.csv'csv HEADER;
+\COPY comments FROM 'C:/Users/Usuaria/intro_dev/programming-intro-lab-14-miasol19651965/data/comentarios.csv'csv HEADER;
+
 
